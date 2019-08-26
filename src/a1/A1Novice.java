7e434d60d81318848d8e8d048dfbf2561859cf49
memus <memus@LAPTOP-JA59DJK6.wireless-1x.unc.edu>
2019-08-26 16:54:14 -0400

    First Attempt at A1 novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,36 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		
+		//get number of customers to be entered
+		int numCustomers = scan.nextInt();
+		
+		
+		//iterate through customers
+		for(int i = 0 ; i < numCustomers ; i++)
+		{
+			//get first/last name and # of items bought
+			String firstName = scan.next();
+			String lastName = scan.next();
+			int numItems = scan.nextInt();
+			
+			double totalPrice = 0;
+			
+			//iterate through items
+			for(int j = 0 ; j < numItems ; j++)
+			{
+				//get quantity, name, and price of item
+				int quantity = scan.nextInt();
+				String name = scan.next();
+				double price = scan.nextDouble();
+				
+				//add cost of items to total
+				totalPrice += quantity * price;
+			}
+			System.out.println(firstName.charAt(0) + ". " + lastName + ": " + String.format("%.2f", totalPrice));
+		}
+		
+		
 		
 	}
 }
